4a1169212848ae5b760a7b7704a3d3be689fe222
MMRYMT <141762507+MMRYMT@users.noreply.github.com>
2023-08-11 10:51:47 +0800

    Add files via upload

---
+++ test1.c
@@ -0,0 +1,15 @@
+#include  <stdio.h>
+  int main()
+  {
+   int a =56;
+   int b =0;
+   int sum =0;
+
+   scanf("%d %d",&a,&b);
+   sum = a +b ;
+
+   printf("sum = %d\n",sum);
+
+   return 0;
+
+  }
