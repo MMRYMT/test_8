5843e29fe26c97e2b842831cd69b1eb45fd14d4c
MMRYMT <141762507+MMRYMT@users.noreply.github.com>
2023-08-21 21:05:15 +0800

    Add files via upload

---
+++ test8.21.20.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+   int i, no;
+
+   printf("请输入一个正整数:");
+   scanf("%d",&no);
+
+   i = 1;
+   while (i <= no)
+          printf("%d ", i++);
+  
+
+   return 0;
+
+
+}
